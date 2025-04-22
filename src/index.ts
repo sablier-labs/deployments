9824a419ce6c9e8c92b019daad597515c62ae919
Paul Razvan Berg <prberg@proton.me>
2025-04-22 17:19:50 +0300

    feat: export helpers

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { default as chains } from "./chains";
+export * from "./helpers";
 export * as indexers from "./indexers";
 export { default as deployments } from "./deployments";
 export * as nativeTokens from "./native-tokens";
