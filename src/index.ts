4cf9067b59da08db311d4c27e5703656853f0b9b
Paul Razvan Berg <prberg@proton.me>
2025-04-28 17:46:17 +0300

    fix: export all releases objects

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 export * from "./helpers";
+export * from "./releases";
 export * from "./types";
 
 export * as chains from "./chains";
 export * as indexers from "./indexers";
 export * as nativeTokens from "./native-tokens";
 export * as versions from "./versions";
-
-export { releases, releasesByProtocol } from "./releases";
