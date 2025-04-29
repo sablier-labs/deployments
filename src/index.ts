83b98b39f7692338469c68c239636b29206da10c
Paul Razvan Berg <prberg@proton.me>
2025-04-29 20:20:37 +0300

    fix: fix chains export

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
+export * from "./chains";
 export * from "./helpers";
 export * from "./releases";
 export * from "./types";
 
-export * as chains from "./chains";
 export * as nativeTokens from "./native-tokens";
